30178ef57118c036aa9f92273222f1c56350c0ac
Honorio Rodriguez <hrodriguez@payall.com.ve>
2021-07-28 16:03:36 -0400

    Agragar Modelos y Servicos

--- proyecto-angular/src/app/components/about/about.component.ts
+++ proyecto-angular/src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
-  constructor() { }
+  constructor() {
+    this.title = "Honorio Rodriguez";
+    this.subtitle = "Desarrollador Web";
+    this.email = "honor29m@gmail.com";
+   }
 
   ngOnInit(): void {
   }
